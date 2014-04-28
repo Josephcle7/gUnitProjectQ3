a2a0beb68a460cb42db07ffe251ed66842ec68ca
Kevin <djinnpenguin@gmail.com>
2014-04-28 14:12:02 -0700

    Create Logs.java
    read initial notes in file

---
+++ Bot/src/Logs.java
@@ -0,0 +1,35 @@
+// Tests follow. These might have to run as an a seperate extension than BotMain. Creating this in case. for now
+// it can act as a medium for notes to implement and test, and serve as a log of sorts for progress on project.
+
+/*		
+
+if (message.equalsIgnoreCase("+"x)) {
+			Integer.parseInt(x);
+			}
+		if (message.equalsIgnoreCase("-"x)) {
+			decrease by x;
+			}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
