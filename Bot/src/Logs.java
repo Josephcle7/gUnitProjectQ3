b10223f48f24e17f3f0646154130849386e5db10
Kevin <djinnpenguin@gmail.com>
2014-04-28 14:26:41 -0700

    Update Logs.java

--- Bot/src/Logs.java
+++ Bot/src/Logs.java
@@ -2,9 +2,20 @@
 // it can act as a medium for notes to implement and test, and serve as a log of sorts for progress on project.
 
 /*		
-
-if (message.equalsIgnoreCase("+"x)) {
+	// arbitrary number. represents channels pitch etc... initializing mid range 0-100
+	int channel = 50
+	
+		if (message.equalsIgnoreCase("+"x)) {
+			
+			//  regex code to change string to only numbers
+			str = str.replaceAll("\\D+","");
+			
+			// this should change the string message into an int
 			Integer.parseInt(x);
+			
+			// then, add it to channel int and change that pitch etc,..
+			int channel = (int channel + int message)
+			
 			}
 		if (message.equalsIgnoreCase("-"x)) {
 			decrease by x;
