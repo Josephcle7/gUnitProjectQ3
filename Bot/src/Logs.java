1edccbcc294dc2566df973395f76a6c0351a1ac9
Kevin <djinnpenguin@gmail.com>
2014-04-28 16:18:19 -0700

    Update Logs.java

--- Bot/src/Logs.java
+++ Bot/src/Logs.java
@@ -5,18 +5,22 @@
 	// arbitrary number. represents channels pitch etc... initializing mid range 0-100
 	int channel = 50
 	
-		if (message.equalsIgnoreCase("+"x)) {
+		if (message.startsWith("+", 0)) {
+			String increase = message.replaceAll("\\D+","");
+			int message = Integer.parseInt(String increase);
+			int channel = (int channel + int message);
+			sendMessage(sender, "Now" + int channel);
+			}
 			
 			//  regex code to change string to only numbers
-			str = str.replaceAll("\\D+","");
+		//	str = str.replaceAll("\\D+","");
 			
 			// this should change the string message into an int
-			Integer.parseInt(x);
+		//	Integer.parseInt(x);
 			
 			// then, add it to channel int and change that pitch etc,..
-			int channel = (int channel + int message)
-			
-			}
+		//	int channel = (int channel + int message)
+		
 		if (message.equalsIgnoreCase("-"x)) {
 			decrease by x;
 			}
