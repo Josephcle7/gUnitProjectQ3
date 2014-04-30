42fa97319826688a0251f23104e88be0a3c761f7
Joshua Kendall <josh-kendall@hotmail.com>
2014-04-29 23:55:49 -0700

    added test bot for me to play around with

---
+++ Bot/src/Logs.java
@@ -0,0 +1,50 @@
+// Tests follow. These might have to run as an a seperate extension than BotMain. Creating this in case. for now
+// it can act as a medium for notes to implement and test, and serve as a log of sorts for progress on project.
+
+/*		
+	// arbitrary number. represents channels pitch etc... initializing mid range 0-100
+	int channel = 50
+	
+		if (message.startsWith("+", 0)) {
+			String increase = message.replaceAll("\\D+","");
+			int message = Integer.parseInt(String increase);
+			int channel = (int channel + int message);
+			sendMessage(sender, "Now" + int channel);
+			}
+			
+			//  regex code to change string to only numbers
+		//	str = str.replaceAll("\\D+","");
+			
+			// this should change the string message into an int
+		//	Integer.parseInt(x);
+			
+			// then, add it to channel int and change that pitch etc,..
+		//	int channel = (int channel + int message)
+		
+		if (message.equalsIgnoreCase("-"x)) {
+			decrease by x;
+			}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
